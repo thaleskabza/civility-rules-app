806ebc332570c75936f5a9eb61c012d56a8ec458
KABELO MODIPA <kabelo.modipa@codicisqa.com>
2025-10-16 16:53:36 +0200

    Update Theme.ts

--- src/domain/entities/Theme.ts
+++ src/domain/entities/Theme.ts
@@ -1,17 +1,17 @@
 export interface Theme {
-    name: string;
-    gradient: string;
-    accent: string;
+  name: string;
+  gradient: string;
+  accent: string;
+}
+
+export class ThemeEntity implements Theme {
+  constructor(
+    public readonly name: string,
+    public readonly gradient: string,
+    public readonly accent: string
+  ) {}
+
+  static fromJson(data: { name: string; gradient: string; accent: string }): ThemeEntity {
+    return new ThemeEntity(data.name, data.gradient, data.accent);
   }
-  
-  export class ThemeEntity implements Theme {
-    constructor(
-      public readonly name: string,
-      public readonly gradient: string,
-      public readonly accent: string
-    ) {}
-  
-    static fromJson(data: any): ThemeEntity {
-      return new ThemeEntity(data.name, data.gradient, data.accent);
-    }
-  }
+}
